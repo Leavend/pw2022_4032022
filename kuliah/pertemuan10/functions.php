520919fdafd5c891d5559ac5fd02e638d3bbe6eb
Leavend <tiohadybayu@gmail.com>
2022-04-05 08:37:45 +0800

    Koneksi DB dan tambil detail data

---
+++ kuliah/pertemuan10/functions.php
@@ -0,0 +1,26 @@
+<?php
+
+function Koneksi()
+{
+  return mysqli_connect('localhost', 'root', '', 'pw_043040023');
+}
+
+function query($query)
+{
+
+  $db = Koneksi();
+
+  $result = mysqli_query($db, $query);
+
+  // jika hasilnya hanya 1 data 
+  if (mysqli_num_rows($result) == 1) {
+    return mysqli_fetch_assoc($result);
+  }
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return $rows;
+}
---
+++ kuliah/pertemuan10/latihan1.php
@@ -0,0 +1,62 @@
+<?php
+// Koneksi Ke DB % Pilih Database
+$db     = mysqli_connect('localhost', 'root', '', 'pw_043040023');
+
+// Query Isi Table Mahasiswa
+$result = mysqli_query($db, "SELECT * FROM mahasiswa");
+
+// Ubah Data Ke array
+// $row    = mysqli_fetch_row($result); // array numerik 
+// $row    = mysqli_fetch_assoc($result); // associative
+// $row    = mysqli_fetch_array($result); // keduanya
+$rows = [];
+while ($row = mysqli_fetch_assoc($result)) {
+  $rows[] = $row;
+}
+
+// Tampung ke Variabel Mahasiswa
+$mahasiswa = $rows;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+
+    <?php $i = 1;
+    foreach ($mahasiswa as $m) : ?>
+      <tr>
+        <td><?= $i++; ?></td>
+        <td><img src="img/<?= $m['gambar']; ?>"></td>
+        <td><?= $m['nrp']; ?></td>
+        <td><?= $m['nama']; ?></td>
+        <td><?= $m['email']; ?></td>
+        <td><?= $m['jurusan']; ?></td>
+        <td>
+          <a href="">ubah</a> | <a href="">hapus</a>
+        </td>
+      </tr>
+    <?php endforeach; ?>
+
+  </table>
+</body>
+
+</html>
